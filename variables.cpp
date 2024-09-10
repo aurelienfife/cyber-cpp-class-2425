e1b9fac8f819ddd86b6518e25d5f41c737e46d44
aurelienfife <161948862+aurelienfife@users.noreply.github.com>
2024-09-10 16:13:43 +0100

    Couple of programs from week1 and week3

---
+++ variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    // Variables
+
+    int age = 42;
+    string name = "jim";
+
+    cout << name << " is " << age << endl;
+
+    return 0;
+}
