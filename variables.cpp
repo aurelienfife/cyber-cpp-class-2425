0fe00a0d6677f37309bb33435779a4387d996609
aurelienfife <161948862+aurelienfife@users.noreply.github.com>
2024-09-10 16:19:03 +0100

    Update variables.cpp

--- variables.cpp
+++ variables.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
     
-    // Variables
+    // // Variables
+    // int age = 42;
+    // string name = "jim";
 
-    int age = 42;
-    string name = "jim";
+    // cout << age << " is " << sizeof(age) << " bytes " << endl;
 
-    cout << name << " is " << age << endl;
+
+    string pass = "password123";
+    string userPassword;
+    
+
+    cout << "Enter your password :";
+    getline(cin, userPassword);
+
+    if (userPassword == pass)
+    {
+        cout << "Password is correct" << endl;
+    }
+    else
+    {
+        cout << "Password is incorrect" << endl;
+    }
+    
+    string ageInput;
+    int age;
+    cout << "how old are you?";
+    getline(cin, ageInput); // Get a string
+    age = stoi(ageInput);
 
     return 0;
+
+
 }
