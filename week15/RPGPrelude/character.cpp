75bbddb767065276b7982d04233a7d4ee2b35151
aurelienfife <161948862+aurelienfife@users.noreply.github.com>
2024-12-17 15:37:53 +0000

    New project using VS22 - github cleanup

---
+++ week15/RPGPrelude/character.cpp
@@ -0,0 +1,9 @@
+#include "character.h"
+
+using namespace std;
+
+Character::Character(const string& _name, int _healthPoints)
+{
+	name = _name;
+	healthPoints = _healthPoints;
+}
---
+++ week15/RPGPrelude/character.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Character
+{
+	// Private data
+	string name;
+	int healthPoints;
+
+public:
+	Character(const string& _name, int _healthPoints);
+};
+
