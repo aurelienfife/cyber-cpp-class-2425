c5173194ef4817afdc9f3b9b0718e9702560ea51
aurelienfife <161948862+aurelienfife@users.noreply.github.com>
2025-02-28 12:53:23 +0000

    Updated, turned into a class

--- RandomNumGenerator/RandomNumGenerator/main.cpp
+++ RandomNumGenerator/RandomNumGenerator/main.cpp
@@ -4,18 +4,47 @@
 
 using namespace std;
 
+// Class "RandomNumberGenerator"
+// Think of it as a lego brick you can use several times in several projects
+class RandomNumberGenerator
+{
+	// Attributes
+	int lastGeneratedNumber;
+
+public:
+	// Constructor -> seeding the generator
+	RandomNumberGenerator() {
+		lastGeneratedNumber = 0;
+		srand(time(0));
+	}
+	// Function -> generates a random int between 0 (incl)
+	// and limit (excl)
+	int generate(int limit) {
+		int randomNum = rand() % limit;
+		// Save
+		lastGeneratedNumber = randomNum;
+
+		return randomNum;
+	}
+	int getLastGeneratedNumber()
+	{
+		return lastGeneratedNumber;
+	}
+};
 
 int main()
 {
-	// W3Schools way
+	// Create a random number generator
 
-	// Seeding the generator
-	srand(time(0));
+	// Create a generator like you would create any variable
+	RandomNumberGenerator g;
 
-	// Generating a random int between 0 and 100
-	int randomNum = rand() % 101;
+	// Create a few numbers
+	for (int i = 0; i < 5; i++) {
+		cout << g.generate(101) << endl;
+	}
 
-	cout << randomNum;
+	cout << "The last generated number was: " << g.getLastGeneratedNumber() << endl;
 
 	return 0;
 }
